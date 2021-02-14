a2bd7a06b3e395eebcb09846738ac23b705c0246
la <laouinia@yahoo.com>
2021-02-13 19:24:30 -0800

    update

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,55 @@
 // product-item.js
-
+const ul = document.getElementById("product-list");
+const createNode = (elem) => {
+  return document.createElement(elem);
+};
+// append an element to parent
+const appendNode = (parent, elem) => {
+  parent.appendChild(elem);
+};
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor() {
+    super();
+
+    let li = createNode("li");
+    let img = createNode("img");
+    let title = createNode("p");
+    let price = createNode("p");
+    let button = createNode("button");
+    button.innerText = "Add to Cart";
+
+    appendNode(li, img);
+    appendNode(li, title);
+    appendNode(li, price);
+    appendNode(li, button);
+    appendNode(ul, li);
+
+    button.onclick = () => {
+      var index = 1;
+      let cart = localStorage.getItem("cart").split(",");
+      if (cart[this.id - index] === "0") {
+        cart[this.id - index] = "1";
+        localStorage.setItem("cart", cart);
+        button.innerHTML = "Remove from Cart";
+        document.getElementById("cart-count").innerHTML++;
+      } else {
+        cart[this.id - index] = "0";
+        localStorage.setItem("cart", cart);
+        button.innerHTML = "Add to Cart";
+        document.getElementById("cart-count").innerHTML--;
+      }
+    };
+    const style = document.createElement("link");
+    style.setAttribute("rel", "stylesheet");
+    style.setAttribute("href", "styles/styles.css");
+    let shadowDOM = this.attachShadow({ mode: "open" });
+    shadowDOM.append(li);
+
+    li.classList.add("product");
+    title.classList.add("title");
+    price.classList.add("price");
+    shadowDOM.append(style);
+  }
 }
 
-customElements.define('product-item', ProductItem);
+customElements.define("product-item", ProductItem);
